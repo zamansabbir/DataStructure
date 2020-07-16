59ba2bc3538cb205b6b44d02cdde4196dd2a958d
zamansabbir <sabbir.zaman@hotmail.com>
2020-07-16 14:30:03 -0500

    Binary Tree: Node insertion added using iterative method

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <queue>
 struct BTree{
     int data;
     BTree* left;
@@ -12,11 +13,55 @@ BTree* createNode(int data){
      return temp;
 }
 
-void insertBTree(BTree* root,int data){
-    BTree* newNode=createNode(data);
+BTree* insertBTree(BTree* root,int data){
+    if(root==NULL){
+        root=createNode(data);
+        return root;
+    }
+    struct BTree* temp;
+    std::queue <BTree*> q;
+    q.push(root);
+    while(!q.empty()){
+        temp=q.front();
+        q.pop();
+        if(temp->left==NULL){
+            temp->left=createNode(data);
+            break;
+        }else{
+            q.push(temp->left);
+        }
+        if(temp->right==NULL){
+            temp->right=createNode(data);
+            break;
+        }else{
+            q.push(temp->right);
+        }
+    }
+    return root;
+}
+
+void traversalBTreeInorder(BTree* root){
+    if(root==NULL){
+        return;
+    }
+    traversalBTreeInorder(root->left);
+    std::cout<<root->data<<" ";
+    traversalBTreeInorder(root->right);
+    
+}
+void traversalBTreePreorder(BTree* root){
 
 }
-int main(){
 
+int main(){
+    BTree* root=NULL;
+    root=insertBTree(root,5);
+    root=insertBTree(root,10);
+    root=insertBTree(root,15);
+    root=insertBTree(root,8);
+    root=insertBTree(root,7);
+    root=insertBTree(root,20);
+    root=insertBTree(root,21);
+    traversalBTreeInorder(root);
     return 0;
 }
